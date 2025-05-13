39402149890ff94511baf76e516bec4bee6b3b35
ShinjiNeo <blackrain5649@gmail.com>
2025-05-13 10:43:17 +0200

    modifica libreria

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-from summarytools import dfSummary
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 
